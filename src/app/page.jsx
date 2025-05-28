1bb6955b87fff9181c14251aa5b2f5d3b4b32cb1
Raunak Singh <raunaksingh@MacBookAir.lan>
2025-05-28 18:52:46 +0530

    new

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -9,22 +9,21 @@ export default function Home() {
   return (
     <div className="flex items-center justify-center w-screen h-screen  flex-col px-4 bg-[var(--bg-color)]">
       <nav className=" fixed top-5 text-white w-[95vw] md:w-[85vw] flex flex-row items-center justify-between">
-    <a href="/" className=" flex flex-row justify-center items-center">
-            <div className="w-[40px] h-[40px] md:w-[60px] md:h-[60px]">
-          <Image  src={"/LOGO.png"} width={60} height={60} alt="logo " />
-        </div>
+        <a href="/" className=" flex flex-row justify-center items-center">
+          <div className="w-[40px] h-[40px] md:w-[60px] md:h-[60px]">
+            <Image src={"/LOGO.png"} width={60} height={60} alt="logo " />
+          </div>
 
-        <h1 className=" text-xl">
-          CIA <b>Labs</b> Dev Route V1
-        </h1></a>
-   
-    <LoginButton />
-        
+          <h1 className=" text-xl">
+            CIA <b>Labs</b> Dev Route V1
+          </h1>
+        </a>
 
+        <LoginButton />
       </nav>
 
       <div className=" text-white">
- <UserGreetText />
+        <UserGreetText />
       </div>
     </div>
   );
--- src/app/providers.jsx
+++ src/app/providers.jsx
@@ -1,7 +1,7 @@
 'use client'
 
 import { createContext, useContext } from 'react'
-import { supabase } from '@/lib/supabase' // ✅ This already contains your configured client
+import { supabase } from '@/lib/auth-actions' // ✅ This already contains your configured client
 
 const SupabaseContext = createContext()
 
