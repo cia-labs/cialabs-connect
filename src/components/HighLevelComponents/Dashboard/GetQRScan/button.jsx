11164ddd8bab11a218e5a91da19e9833f9e114e0
Raunak Singh <raunaksingh@MacBookAir.lan>
2025-05-30 18:31:37 +0530

    fix4

--- src/components/HighLevelComponents/Dashboard/GetQRScan/button.jsx
+++ src/components/HighLevelComponents/Dashboard/GetQRScan/button.jsx
@@ -1,23 +1,26 @@
-import React from "react";
+import React, {useEffect} from "react";
 import QrCodeRoundedIcon from "@mui/icons-material/QrCodeRounded";
 import CropFreeRoundedIcon from "@mui/icons-material/CropFreeRounded";
-//import { useRouter } from "next/navigation";
-import Link from 'next/link';
+import { useRouter } from "next/navigation";
 
 export default function GetQrScanQrBTN({uid}) {
-  //const router = useRouter();
-  // const handleClickOR = () => {
-  //   router.push(`/dashboard/user/${uid}/QR`);
-  // };
+  const router = useRouter();
+    useEffect(() => {
+  router.prefetch(`/dashboard/user/${uid}/QR`);
+}, [uid]);
+  const handleClickOR = () => {
+    router.push(`/dashboard/user/${uid}/QR`);
+  };
+
   return (
     <div className="fixed bottom-14 z-40 backdrop-blur left-1/2 -translate-x-1/2 w-[60vw] lg:w-[30vw] h-[7vh] border border-[#363535] rounded-[30px] flex flex-row">
-      <Link
-        href={`/dashboard/user/${uid}/QR`}
-        //onClick={handleClickOR}
+      <button
+        //href={`/dashboard/user/${uid}/QR`}
+        onClick={handleClickOR}
         className="w-[50%] h-full text-[#a1a1a1] transition-all  active:text-white active:text-lg flex flex-row justify-center items-center gap-2"
       >
         Get QR <QrCodeRoundedIcon />
-      </Link>
+      </button>
       <button  className="w-[50%] h-full text-black/70  transition-all active:text-lg active:text-black flex flex-row justify-center items-center gap-2 bg-[#C2F970] rounded-r-[30px] ">
         Scan <CropFreeRoundedIcon />
       </button>
