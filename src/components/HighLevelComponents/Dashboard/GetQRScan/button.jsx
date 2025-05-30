7747e3e414da853aad873ef6aa8543e36bcb2a6b
Raunak Singh <raunaksingh@MacBookAir.lan>
2025-05-30 18:26:28 +0530

    link check2

--- src/components/HighLevelComponents/Dashboard/GetQRScan/button.jsx
+++ src/components/HighLevelComponents/Dashboard/GetQRScan/button.jsx
@@ -1,19 +1,19 @@
 import React from "react";
 import QrCodeRoundedIcon from "@mui/icons-material/QrCodeRounded";
 import CropFreeRoundedIcon from "@mui/icons-material/CropFreeRounded";
-import { useRouter } from "next/navigation";
+//import { useRouter } from "next/navigation";
 import Link from 'next/link';
 
 export default function GetQrScanQrBTN({uid}) {
-  const router = useRouter();
-  const handleClickOR = () => {
-    router.push(`/dashboard/user/${uid}/QR`);
-  };
+  //const router = useRouter();
+  // const handleClickOR = () => {
+  //   router.push(`/dashboard/user/${uid}/QR`);
+  // };
   return (
     <div className="fixed bottom-14 z-40 backdrop-blur left-1/2 -translate-x-1/2 w-[60vw] lg:w-[30vw] h-[7vh] border border-[#363535] rounded-[30px] flex flex-row">
       <Link
         href={`/dashboard/user/${uid}/QR`}
-        onClick={handleClickOR}
+        //onClick={handleClickOR}
         className="w-[50%] h-full text-[#a1a1a1] transition-all  active:text-white active:text-lg flex flex-row justify-center items-center gap-2"
       >
         Get QR <QrCodeRoundedIcon />
