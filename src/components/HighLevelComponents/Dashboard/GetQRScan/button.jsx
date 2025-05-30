655e9975890ad8ad121f44cfc723b32cb33f552f
Raunak Singh <raunaksingh@Raunaks-MacBook-Air.local>
2025-05-30 18:17:14 +0530

    mob fix test

--- src/components/HighLevelComponents/Dashboard/GetQRScan/button.jsx
+++ src/components/HighLevelComponents/Dashboard/GetQRScan/button.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import QrCodeRoundedIcon from "@mui/icons-material/QrCodeRounded";
 import CropFreeRoundedIcon from "@mui/icons-material/CropFreeRounded";
 import { useRouter } from "next/navigation";
+import Link from 'next/link';
 
 export default function GetQrScanQrBTN({uid}) {
   const router = useRouter();
@@ -10,15 +11,16 @@ export default function GetQrScanQrBTN({uid}) {
   };
   return (
     <div className="fixed bottom-14 z-40 backdrop-blur left-1/2 -translate-x-1/2 w-[60vw] lg:w-[30vw] h-[7vh] border border-[#363535] rounded-[30px] flex flex-row">
-      <button
+      <Link
+        href={`/dashboard/user/${uid}/QR`}
         onClick={handleClickOR}
-        className="w-[50%] h-full text-[#a1a1a1] transition-all active:text-lg active:text-white active:text-lg flex flex-row justify-center items-center gap-2"
+        className="w-[50%] h-full text-[#a1a1a1] transition-all  active:text-white active:text-lg flex flex-row justify-center items-center gap-2"
       >
         Get QR <QrCodeRoundedIcon />
-      </button>
-      <button className="w-[50%] h-full text-black/70 active:text-lg transition-all active:text-lg active:text-black flex flex-row justify-center items-center gap-2 bg-[#C2F970] rounded-r-[30px] ">
+      </Link>
+      <Link className="w-[50%] h-full text-black/70  transition-all active:text-lg active:text-black flex flex-row justify-center items-center gap-2 bg-[#C2F970] rounded-r-[30px] ">
         Scan <CropFreeRoundedIcon />
-      </button>
+      </Link>
     </div>
   );
 }
