3d84d136540d131859c629d63c06d93b1fddcb27
Abhiraj1929 <officialabhiraj1907@gmail.com>
2025-10-24 17:24:53 +0530

    upcomingevent

---
+++ src/app/upcoming-events/page.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import UpcomingEvents from '@/components/LandingComponents/upcomingEvents';
+const page = () => {
+  return (
+      <UpcomingEvents/>
+  )
+}
+
+export default page
